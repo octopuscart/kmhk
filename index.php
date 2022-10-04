6473d26f58319602df5959ad3bf9728865d01389
octopuscart <octopuscartltd@gmail.com>
2022-10-04 18:34:55 +0530

    add files

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <!-- Mirrored from startbootstrap.github.io/startbootstrap-new-age/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 04 Oct 2022 10:05:55 GMT -->
+    <!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title> Kamran Int'l Trade Ltd Hong Kong</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="./cdn.jsdelivr.net/npm/bootstrap-icons%401.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link rel="preconnect" href="https://fonts.gstatic.com/" />
+        <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,600;1,600&amp;display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&amp;display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow-sm" id="mainNav">
+            <div class="container px-5">
+                <a class="navbar-brand fw-bold" href="#page-top"><img src="images/logo.png" style="    height: 50px;" /></a>
+                <button class="navbar-toggler  text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="bi-list"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
+                        <li class="nav-item"><a class="nav-link me-lg-3 text-white" href="#home">Home</a></li>
+                        <li class="nav-item"><a class="nav-link me-lg-3 text-white" href="#aboutus">About Us</a></li>
+                        <li class="nav-item"><a class="nav-link me-lg-3 text-white" href="#products">Our Products</a></li>
+                        <li class="nav-item"><a class="nav-link me-lg-3 text-white" href="#contact_us">Contact Us</a></li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+        <!-- Mashead header-->
+        <header class="masthead" id="home">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-6">
+                        <!-- Mashead text and app badges-->
+                        <div class="mb-5 mb-lg-0 text-center text-lg-start">
+                            <h1 class="display-1 lh-1 mb-3">Our objective is to long-standing relationships</h1>
+                            <p class="lead fw-normal text-muted mb-5">We are forward looking company with the aim of building upon strong relationships based on trust and performance</p>
+
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <!-- Masthead device mockup feature-->
+                        <div class="masthead-device-mockup">
+                            <svg class="circle" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                            <defs>
+                            <linearGradient id="circleGradient" gradientTransform="rotate(45)">
+                            <stop class="gradient-start-color" offset="0%"></stop>
+                            <stop class="gradient-end-color" offset="100%"></stop>
+                            </linearGradient>
+                            </defs>
+                            <circle cx="50" cy="50" r="50"></circle></svg
+                            ><svg class="shape-1 d-none d-sm-block" viewBox="0 0 240.83 240.83" xmlns="http://www.w3.org/2000/svg">
+                            <rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03" transform="translate(120.42 -49.88) rotate(45)"></rect>
+                            <rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03" transform="translate(-49.88 120.42) rotate(-45)"></rect></svg
+                            ><svg class="shape-2 d-none d-sm-block" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="50"></circle></svg>
+                            <div class="device-wrapper">
+                                <div class="device" data-device="iPhoneX" data-orientation="portrait" data-color="black">
+                                    <div class="screen bg-black">
+                                        <!-- PUT CONTENTS HERE:-->
+                                        <!-- * * This can be a video, image, or just about anything else.-->
+                                        <!-- * * Set the max width of your media to 100% and the height to-->
+                                        <!-- * * 100% like the demo example below.-->
+                                        <video muted="muted" autoplay="" loop="" style="max-width: 100%; height: 100%"><source src="assets/img/demo-screen.mp4" type="video/mp4" /></video>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <!-- App features section-->
+        <section id="aboutus" class="bg-gradient">
+            <div class="container">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-8 order-lg-1 mb-5 mb-lg-0">
+                        <div class="container-fluid " style="padding-left: 5rem !important;">
+                            <h3 class="text-dark display-4 lh-1 mb-4">
+                                About Us
+                            </h3>
+                            <p> Kamran International Trade Limited. is forward looking company with the aim of building upon strong
+                                relationships in established and emerging markets that were built during many years in the telecommunication business by our sister concerns companies.
+                            </p>
+                            <p>Kamran International Trade Limited team consists of prominent professionals from the Mobile
+                                phone industry in the Hong Kong China mainland UK and United States.
+                                This team has a collective experience of over Sixteen years in this young, fast paced industry.</p>
+
+                            <p>Kamran International Trade Limited has deep and strong relationships with suppliers and buyers
+                                based in China Mainland, Hong Kong, UK, United States, Continental Europe, and the
+                                Middle East.</p>
+
+                            <p>We are exporting product to worldwide, we have recently also started to make significant inroads into the markets of Australia, Newzealand,Eastern Europe, North Africa, India, Sri Lanka, Pakistan,
+                                Afghanistan, and Bangladesh.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 order-lg-0">
+                        <!-- Features section device mockup-->
+                        <img src="./images/about.webp" class="img-fluid" />
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Quote/testimonial aside-->
+        <aside class="text-center bg-gradient-primary-to-secondary">
+            <div class="container px-5">
+                <div class="row gx-5 justify-content-center">
+                    <div class="col-xl-8">
+                        <div class="h2 fs-1 text-white mb-4">Kamran Int'l Trade Limited is holding External Telecommunications Services (ETS) Operators license from Office of the Communication Authority Hong Kong.
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </aside>
+
+        <!-- App features section-->
+        <section id="products">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                     <h3 class="text-dark display-4 lh-1 mb-4 text-center">
+                                Our Products
+                            </h3>
+                    <div class="col-lg-12 order-lg-1 mb-5 mb-lg-0">
+                        <div class="container-fluid px-5">
+                            <div class="row gx-5">
+                                <div class="col-md-6 mb-5">
+                                    <!-- Feature item-->
+                                    <div class="text-center">
+                                        <i class="bi-phone icon-feature text-gradient d-block mb-3"></i>
+                                        <h3 class="font-alt">GSM VoIP Gateway</h3>
+                                        <p class="text-muted mb-0">Gsm Gateway Provider makes it possible to make external calls over the GSM network.</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 mb-5">
+                                    <!-- Feature item-->
+                                    <div class="text-center">
+                                        <i class="bi-braces icon-feature text-gradient d-block mb-3"></i>
+                                        <h3 class="font-alt">GoIP VoIP Gateway</h3>
+                                        <p class="text-muted mb-0">GOIP8 VOIP Gateway is a famous product that connect the GSM and the VOIP seamlessly.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6 mb-5 mb-md-0">
+                                    <!-- Feature item-->
+                                    <div class="text-center">
+                                        <i class="bi-circle-fill icon-feature text-gradient d-block mb-3"></i>
+                                        <h3 class="font-alt">CDR 700MB</h3>
+                                        <p class="text-muted mb-0">100 high-grade non-rewritable CD-R discs with one hundred year archival life.</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <!-- Feature item-->
+                                    <div class="text-center">
+                                        <i class="bi-circle icon-feature text-gradient d-block mb-3"></i>
+                                        <h3 class="font-alt">DVD-R DVD+R</h3>
+                                        <p class="text-muted mb-0">A DVD+R is an optical, write-once recordable disk with 4.6 gigabyte storage capacity. It is similar to a DVD-R format.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+   
+     
+        <!-- App badge section-->
+        <section class="bg-gradient-primary-to-secondary" id="contact_us">
+            <div class="container px-5">
+                <h2 class="text-center text-white font-alt mb-4">Contact Us</h2>
+                <div class="align-items-center text-center text-white">
+                    <b>KAMRAN  INTERNATIONAL  TRADE  LIMITED</b>
+                    <p>UNIT C, BLOCK 5, 46/F, CARIBBEAN COAST, <br/>
+                        NO.2 KIN TUNG ROAD, TUNG CHUNG, HONG KONG </p>
+                    <p>Email: enquiry@kamran.com.hk</p>
+
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="bg-black text-center py-5">
+            <div class="container px-5">
+                <div class="text-white-50 small">
+                    <div class="mb-2">&copy; www.kamran.com.hk <?php echo date("Y")?>. All Rights Reserved.</div>
+                  
+                </div>
+            </div>
+        </footer>
+
+        <!-- Bootstrap core JS-->
+        <script src="./cdn.jsdelivr.net/npm/bootstrap%405.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="./cdn.startbootstrap.com/sb-forms-0.4.1.js"></script>
+    </body>
+
+    <!-- Mirrored from startbootstrap.github.io/startbootstrap-new-age/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 04 Oct 2022 10:05:59 GMT -->
+</html>
